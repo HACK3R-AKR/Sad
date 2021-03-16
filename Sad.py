c2db1381a1085f55163db7fcca91db445a9910b0
HACK3R-AKR <80332174+HACK3R-AKR@users.noreply.github.com>
2021-03-16 18:12:42 +0530

    Add files via upload

---
+++ Sad.py
@@ -0,0 +1,30 @@
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+name=input(f"{GG}BOY  : ")
+print(f"{C}\nGIRL : Hadaa Parra\n")
+name=input(f"{GG}BOY  : ")
+print(f"{C}\nGIRL : Sorry inik realtion onnum thalparyam illya\n")
+name=input(f"{GG}BOY  : ")
+print(f"{C}\nGIRL : Daa ank vishamam ayyo\n")
+name=input(f"{GG}BOY  : ")
+print(f"{C}\nGIRL : Sorry too\n")
+name=input(f"{GG}BOY  : ")
+print(f"{C}\nGIRL : Nna sheri njan povva\n")
+name=input(f"{GG}BOY  : ")
+print(f"{C}\nGIRL : Mmm\n")
